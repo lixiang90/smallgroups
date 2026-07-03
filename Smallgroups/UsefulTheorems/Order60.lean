dc641c230c4b501de737c9d0947a8292e0b31637
lixiang90 <lx20150211@gmail.com>
2026-07-04 02:09:17 +0800

    rename file

--- Smallgroups/UsefulTheorems/Order60.lean
+++ Smallgroups/UsefulTheorems/Order60.lean
@@ -3,4 +3,4 @@ Copyright (c) 2026 Smallgroups contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Smallgroups contributors
 -/
-import Smallgroups.UsefulTheorems.Order60.Order60_simple
+import Smallgroups.UsefulTheorems.Order60.A5
