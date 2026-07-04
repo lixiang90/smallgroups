0181865d09baf301da3c796ea6483b44e171454c
lixiang90 <lx20150211@gmail.com>
2026-07-04 22:49:47 +0800

    begin to handle order 80 : either Sylow-5 or Sylow-2 is unique

--- Smallgroups.lean
+++ Smallgroups.lean
@@ -38,8 +38,7 @@ import Smallgroups.UsefulTheorems.Order40
 import Smallgroups.UsefulTheorems.Order54
 import Smallgroups.UsefulTheorems.Order56
 import Smallgroups.UsefulTheorems.Order60
-import Smallgroups.UsefulTheorems.Order60.Semiproduct
-import Smallgroups.UsefulTheorems.Order60.Classification
+import Smallgroups.UsefulTheorems.Order80
 import Smallgroups.UsefulTheorems.Order63
 import Smallgroups.UsefulTheorems.Order84
 import Smallgroups.UsefulTheorems.Order88
